308606d2cec0e2af9c2900f02b1f384d13d02e80
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-17 00:08:28 +0600

    2/17/25

--- pixel-positions/resources/views/components/layout.blade.php
+++ pixel-positions/resources/views/components/layout.blade.php
@@ -5,18 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Position</title>
-    @vite(['resources/js/app.js'])
+    @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body>
     <div>
 
-        <nav>
+        <nav class="flex justify-between items-center p-4">
             <div>
                 <a href="">
                     <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
                 </a>
             </div>
-            <div>links</div>
+
+            <div class="flex space-x-4">
+                <a href="">Job</a>
+                <a href="">Careers</a>
+                <a href="">Salaries</a>
+                <a href="">Companies</a>
+            </div>
+
             <div>post a job</div>
         </nav>
 
--- pixel-positions/tailwind.config.js
+++ pixel-positions/tailwind.config.js
@@ -2,13 +2,18 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
+    // content: [
+    //     './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    //     './storage/framework/views/*.php',
+    //     './resources/**/*.blade.php',
+    //     './resources/**/*.js',
+    //     './resources/**/*.vue',
+    // ],
+    plugins: [
+        tailwindcss(),
         './resources/**/*.blade.php',
         './resources/**/*.js',
-        './resources/**/*.vue',
-    ],
+      ],
     theme: {
         extend: {
             fontFamily: {
