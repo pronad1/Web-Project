85cc4005631000b03709aca326d2ee0bed52c82a
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-18 01:00:07 +0600

    2/18/25

--- pixel-positions/app/Models/Job.php
+++ pixel-positions/app/Models/Job.php
@@ -11,10 +11,13 @@ class Job extends Model
     /** @use HasFactory<\Database\Factories\JobFactory> */
     use HasFactory;
 
-    public function tag()
+    public function tag(string $name)
     {
+        $tag = Tag::firstOrCreate(['name' => $name]);
 
+        $this->tags()->attach($tag);
     }
+
     public function tags()
     {
         return $this->belongsToMany(Tag::class);
--- pixel-positions/database/migrations/2025_02_17_181651_create_job_tag_table.php
+++ pixel-positions/database/migrations/2025_02_17_181651_create_job_tag_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Job;
+use App\Models\Tag;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('job_tag', function (Blueprint $table) {
             $table->id();
+            $table->foreignId(Job::class)->constrained()->cascadeOnDelete();
+            $table->foreignId(Tag::class)->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
