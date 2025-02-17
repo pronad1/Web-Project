342f9b9d45d80d2a6a8a495d282aada70c7e7e3c
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-17 21:59:07 +0600

    2/17/25

--- pixel-positions/app/Models/Job.php
+++ pixel-positions/app/Models/Job.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Employee;
 
 class Job extends Model
 {
     /** @use HasFactory<\Database\Factories\JobFactory> */
     use HasFactory;
+
+    public function employees()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
--- pixel-positions/app/Models/User.php
+++ pixel-positions/app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Employee; // Ensure this class exists in the specified namespace
 
 class User extends Authenticatable
 {
@@ -44,5 +45,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function employee()
+    {
+        return $this->hasOne(Employer::class);
     }
 }
