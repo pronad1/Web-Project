e923b1b6f18a79723ae4af2d800c7ab936504e5f
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-18 00:16:05 +0600

    2/18/25

--- pixel-positions/app/Models/Job.php
+++ pixel-positions/app/Models/Job.php
@@ -11,6 +11,15 @@ class Job extends Model
     /** @use HasFactory<\Database\Factories\JobFactory> */
     use HasFactory;
 
+    public function tag()
+    {
+
+    }
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+    
     public function employees()
     {
         return $this->belongsTo(Employer::class);
