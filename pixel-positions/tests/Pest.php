1bc6d8dfaa952428054913d32653d36189dddefa
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-19 22:14:40 +0600

    2/19/25

--- pixel-positions/tests/Pest.php
+++ pixel-positions/tests/Pest.php
@@ -11,11 +11,12 @@
 |
 */
 
-pest()->extend(Tests\TestCase::class)
- // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
-    ->in('Feature');
+uses(
+    Tests\TestCase::class,
+    Illuminate\Foundation\Testing\RefreshDatabase::class,
+)->in('Feature', 'Unit');
 
-/*
+/*   
 |--------------------------------------------------------------------------
 | Expectations
 |--------------------------------------------------------------------------
