0e7c2884c9db1e1a579bad2b372a7356466afa2e
pronad1 <ug2102049@cse.pstu.ac.bd>
2025-02-20 18:10:58 +0600

    2/20/25

--- pixel-positions/resources/views/components/layout.blade.php
+++ pixel-positions/resources/views/components/layout.blade.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
+    <link rel="stylesheet" href="{{ asset('output.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
